fffab23d70c13f9f62b56fd5a13996453d41c23e
Pablo <pablo.catanor@upb.edu.co>
2022-10-03 21:13:05 -0500

    Cambio de escenas + vídeos implementados

---
+++ Assets/scripts/VideoSwitch2.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoSwitch2 : MonoBehaviour
+{
+
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(3);//the scene that you want to load after the video has ended.
+    }
+}
---
+++ Assets/scripts/VideoSwitchInicio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoSwitchInicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(1);//the scene that you want to load after the video has ended.
+    }
+}
